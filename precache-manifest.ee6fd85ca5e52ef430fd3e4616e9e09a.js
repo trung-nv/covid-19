6945c28bc0c6d1a04afc8bbce3a35ac7a8e444e7
trung-nv <trung.nv@hotmail.com>
2020-04-13 12:43:27 +0700

    Updates

---
+++ precache-manifest.ee6fd85ca5e52ef430fd3e4616e9e09a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32758cc598d854ead74c054ab873f538",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "a7c06b461baec9f9cce2",
+    "url": "/covid-19/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "865d9591011392c7a3bb",
+    "url": "/covid-19/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "a7c06b461baec9f9cce2",
+    "url": "/covid-19/static/js/2.50eda5f8.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/covid-19/static/js/2.50eda5f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "865d9591011392c7a3bb",
+    "url": "/covid-19/static/js/main.80ad71fc.chunk.js"
+  },
+  {
+    "revision": "e5089d81195f500b7df8",
+    "url": "/covid-19/static/js/runtime-main.0ff6cc0f.js"
+  }
+]);
